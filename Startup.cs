941b8235a1b71e0612c9de6f694f477468e86158
Minsu <ryualstn@naver.com>
2021-07-15 17:05:50 +0900

    2nd

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace JenkinsWithDotnet
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello Minsu!");
+                    await context.Response.WriteAsync("Hello Minsu2!");
                 });
             });
         }
